dbb5b2a378526b14df553b582fc87360bcacdef1
Muhammad Anas Hasan <anashasan09@gmail.com>
2020-08-23 23:44:10 +0500

    testapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCorejenkinsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
